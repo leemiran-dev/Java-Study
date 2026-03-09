8a035a362df049ece84581122a30358c90e3d6bf
leemiran-dev <leemiran.0729@gmail.com>
2026-03-09 16:06:57 +0900

    JAVA OOP: 상속 오버라이딩 예시

--- src/main/java/com/example/javastudy/oop/Admin.java
+++ src/main/java/com/example/javastudy/oop/Admin.java
@@ -1,6 +1,11 @@
 package com.example.javastudy.oop;
 
 public class Admin extends User{
+    // 오버라이딩 -> User 부모 클래스의 setId와 별개
+    void setId(String param) {
+        this.id = param;
+        System.out.println("admin ID 설정 되었습니다");
+    }
     void reviewHistory() {
         System.out.println("히스토리 검토중... (사용 중인 id: "+this.id+")");
     }
