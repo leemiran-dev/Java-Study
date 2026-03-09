5e4d6be63b2eb9a397db49651a9b1458216f60b0
leemiran-dev <leemiran.0729@gmail.com>
2026-03-09 13:27:17 +0900

    JAVA 제어문: 반복문 while 예시

---
+++ src/main/java/com/example/javastudy/controlstatement/While.java
@@ -0,0 +1,12 @@
+package com.example.javastudy.controlstatement;
+
+public class While {
+    public static void main(String[] args) {
+        int year = 2020;
+
+        while(year < 2027) {
+            System.out.println("현재 연도는 "+year+"년 입니다.");
+            year++;
+        }
+    }
+}
